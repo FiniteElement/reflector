bc464acbd918a60a4da1eaafcf85064382b35929
FiniteElement <finiteelement@outlook.hu>
2015-04-05 20:19:38 +0200

    Class Test is moved to separate file.

--- Reflector/Program.cs
+++ Reflector/Program.cs
@@ -2,27 +2,6 @@
 
 namespace Reflector
 {
-	class Test
-	{
-		public String Member = "Simple member!";
-
-		public Test[] Elements { get; set; }
-
-		public Test Property { get; set; }
-
-		public String Leaf { get; set; }
-
-		public String Method ()
-		{
-			return "Method!";
-		}
-
-		public String MethodWithParam (int a)
-		{
-			return a.ToString ();
-		}
-	}
-
 	class MainClass
 	{
 		public static void Main (string[] args)
--- Reflector/Test.cs
+++ Reflector/Test.cs
@@ -4,8 +4,22 @@ namespace Reflector
 {
 	public class Test
 	{
-		public Test ()
+		public String Member = "Simple member!";
+
+		public Test[] Elements { get; set; }
+
+		public Test Property { get; set; }
+
+		public String Leaf { get; set; }
+
+		public String Method ()
 		{
+			return "Method!";
+		}
+
+		public String MethodWithParam (int a)
+		{
+			return a.ToString ();
 		}
 	}
 }
