98e8a9ec339131c247be50a6e684bb79f49c7f29
FiniteElement <finiteelement@outlook.hu>
2015-04-05 20:21:42 +0200

    ReflectorTest is included in the solution.

--- ReflectorTest/ReflectorTest.cs
+++ ReflectorTest/ReflectorTest.cs
@@ -1,14 +1,45 @@
 ﻿using NUnit.Framework;
-using System;
+using Reflector;
 
 namespace ReflectorTest
 {
-	[TestFixture ()]
-	public class Test
+	[TestFixture]
+	public class ReflectorTest
 	{
-		[Test ()]
-		public void TestCase ()
+		[Test]
+		public void CreateTypedParameterlessRoot()
 		{
+			var root = Reflector.Reflector.CreateRootReflector<Test> ();
+			Assert.IsNotNull (root);
+		}
+
+		[Test]
+		public void CreateTypedParametrizedRoot()
+		{
+			var root = Reflector.Reflector.CreateRootReflector<Test> (new Test());
+			Assert.IsNotNull (root);
+		}
+
+		[Test]
+		public void CreateParametrizedRoot()
+		{
+			var root = Reflector.Reflector.CreateRootReflector (new Test());
+			Assert.IsNotNull (root);
+		}
+
+
+		[Test]
+		public void CreateTypedNullParametrizedRoot()
+		{
+			var root = Reflector.Reflector.CreateRootReflector<Test> (null);
+			Assert.IsNull (root);
+		}
+
+		[Test]
+		public void CreateNullParametrizedRoot()
+		{
+			var root = Reflector.Reflector.CreateRootReflector (null);
+			Assert.IsNull (root);
 		}
 	}
 }
